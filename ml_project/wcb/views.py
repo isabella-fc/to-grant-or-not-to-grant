5fee324d51902e3969663d4dc448329e8fdf3344
isafc12 <79331214+isabella-fc@users.noreply.github.com>
2024-12-10 17:42:27 +0000

    updated model path

--- ml_project/wcb/views.py
+++ ml_project/wcb/views.py
@@ -1,42 +1,45 @@
+import os
 from django.shortcuts import render
+from django.conf import settings
 from .models import Predictions
 from .forms import ModelForm
 import pickle
 
 def predict_model(request):
-    # if this is a POST request we need to process the form data
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
         form = ModelForm(request.POST)
-        # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
             sepal_length = form.cleaned_data['sepal_length']
             sepal_width = form.cleaned_data['sepal_width']
             petal_length = form.cleaned_data['petal_length']
             petal_width = form.cleaned_data['petal_width']
 
-            # Run new features through ML model
-            model_features = [
-                [sepal_length, sepal_width, petal_length, petal_width]]
-                
-            loaded_model = pickle.load(
-                open("ml_project\ml_model\wcb_model.pkl", 'rb'))
+            model_features = [[sepal_length, sepal_width, petal_length, petal_width]]
+
+            # Construct the model path
+            model_path = os.path.join(settings.BASE_DIR, 'ml_project', 'ml_model', 'wcb_model.pkl')
+            print(f"Resolved model path: {model_path}")  # Debugging
+
+            if not os.path.exists(model_path):
+                raise FileNotFoundError(f"Model file not found at: {model_path}")
+
+            with open(model_path, 'rb') as model_file:
+                loaded_model = pickle.load(model_file)
+
             prediction = loaded_model.predict(model_features)[0]
-
             prediction_name_list = ['setosa', 'versicolor', 'virginica']
-
             prediction_name = prediction_name_list[prediction]
 
-            Predictions.objects.create(sepal_length=sepal_length,
-                            sepal_width=sepal_width,
-                            petal_length=petal_length,
-                            petal_width=petal_width,
-                            prediction=prediction_name)
+            Predictions.objects.create(
+                sepal_length=sepal_length,
+                sepal_width=sepal_width,
+                petal_length=petal_length,
+                petal_width=petal_width,
+                prediction=prediction_name
+            )
 
             return render(request, 'home.html', {'form': form, 'prediction': prediction_name})
 
-    # if a GET (or any other method) we'll create a blank form
     else:
         form = ModelForm()
 
