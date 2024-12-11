54cf81c52cd36e0e13c7ac43c00af25523631327
Isabella Figueredo Costa <20240685@novaims.unl.pt>
2024-12-11 12:07:15 +0000

    remove secret key

--- ml_project/ml_project/settings.py
+++ ml_project/ml_project/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-print(f'secret_key: {os.getenv('SECRET_KEY')}')
-
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
