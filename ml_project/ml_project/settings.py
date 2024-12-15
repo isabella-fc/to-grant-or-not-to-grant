54e7d6ed6669e68c227d3aa1c25b4adbb2c486a6
isafc12 <79331214+isabella-fc@users.noreply.github.com>
2024-12-15 23:10:00 +0000

    print root

--- ml_project/ml_project/settings.py
+++ ml_project/ml_project/settings.py
@@ -121,9 +121,11 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+print(STATIC_ROOT)
 
 STATICFILES_DIRS = [BASE_DIR / "static",]
 
