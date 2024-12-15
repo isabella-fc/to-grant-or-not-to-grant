feb919e39971f51c8901f4320d6730f469ac7385
isafc12 <79331214+isabella-fc@users.noreply.github.com>
2024-12-15 23:57:17 +0000

    rollback

--- ml_project/ml_project/settings.py
+++ ml_project/ml_project/settings.py
@@ -127,7 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 print(STATIC_ROOT)
 
-STATICFILES_DIRS = [BASE_DIR / "static",]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
